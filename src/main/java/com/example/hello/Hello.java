ee73a499b46900c4db42e35040f10fd857433448
Dhruv Agarwal <dhruv9837758333@gmail.com>
2025-03-01 15:21:21 +0530

    Added Request Params

--- src/main/java/com/example/hello/Hello.java
+++ src/main/java/com/example/hello/Hello.java
@@ -1,13 +1,25 @@
 package com.example.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class Hello {
 
-    @RequestMapping("/")
-    public String greet() {
-        return "Hello from Bridgelabz!!!";
+
+
+    //uc1
+    @GetMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello From BridgeLabz! ! !";
+
+    }
+    // UC2
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello " + name+" from Bridgelabcz";
     }
 }
