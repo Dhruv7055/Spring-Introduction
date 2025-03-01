86a99e8d50be71ad61d7a088e23d13c18e35e5cf
Dhruv Agarwal <dhruv9837758333@gmail.com>
2025-03-01 17:40:24 +0530

    Added PostMethod

--- src/main/java/com/example/hello/controllers/Hello.java
+++ src/main/java/com/example/hello/controllers/Hello.java
@@ -1,4 +1,5 @@
 package com.example.hello.controllers;
+import com.example.hello.model.User;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +20,18 @@ public class Hello {
     public String sayHello(@RequestParam String name){
         return "Hello " + name+"Thanks for using my application";
     }
+
     // UC3 - PathParams
     @GetMapping("/{name}")
     public String sayHelloPath(@PathVariable String name){
         return "Hello " + name+"Thanks for using my application";
+    }
+
+    // UC4 - PostMethod: Handles POST requests at "/post" with a User object in the request body.
+    // URL: Use POST request at http://localhost:8080/post with JSON body: { "firstName": "Dhruv", "lastName": "Agarwal" }
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " Thanks for using my application";
     }
 
 }
